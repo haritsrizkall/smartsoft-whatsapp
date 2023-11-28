5c30f6c5d61b7cabcb8c8a2fc79b8372b49aba4c
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2023-11-28 21:44:11 +0700

    key env

--- src/lib/wa.ts
+++ src/lib/wa.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const sendWa = async (no_wa: string, message: string) => {
   const resp = await axios.post(
-    `https://graph.facebook.com/v17.0/112300485084181/messages`,
+    `https://graph.facebook.com/v17.0/${process.env.FB_NO_ID}/messages`,
     {
       "messaging_product": 'whatsapp',
       "recipient_type": 'individual',
@@ -16,7 +16,7 @@ export const sendWa = async (no_wa: string, message: string) => {
     {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer EAAQY6kxUz74BO6pWHeWMON1USyi1R1U2zL3Du1fY3YB5jGJSLuERjeJ7yC09wlUcsNpz28p9lStR5VZBYAPYHStFtaVCgBcaFZCdiamoilyh38P1EoplFZACClPVtK5I6nfuiUwNCadrYGx1gDStoz0ZA3wYZCXorCynGHME1MyGEf8184kC86Wm1c9DgPlSFu5d3D1jAuyLFDKHTrqBWqQGAJZCkZD'
+        'Authorization': `Bearer ${process.env.FB_API_KEY}`
       }
     }
   )
